32bad3b51617dc1a20aa140737157a5c64ad4444
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-07-04 17:27:48 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:alpine3.10
+ARG CLI_VERSION=1.16.190
+
+# get AWS CLI and its dependencies (groff)
+RUN apk -uv add --no-cache groff
+RUN pip install --no-cache-dir awscli=="${CLI_VERSION}"
+
+RUN mkdir -p /.aws/cli/cache && \
+    chown -R nobody:nobody /.aws && \
+    chmod -R a+rwx /.aws
+
+# set up /etc/profile
+RUN mv /usr/local/bin/aws /usr/bin/aws
+COPY --chown=nobody:nobody aws.sh /usr/local/bin/aws
+RUN chmod a+x /usr/local/bin/aws
---
+++ aws.sh
@@ -0,0 +1,44 @@
+#!/bin/sh -eu
+
+# parameters
+arnOfRoleToAssume=$ASSUME_ROLE_ARN
+
+# naming
+initialProfileName=initial # the profile with credentials we use to assume the role
+assumeRoleProfileName=assume_role # the sub-profile that forces the role-assumption
+
+# generate shared credentials file for env-defined AWS credentials
+awsSharedCredentialsFileForInitialCredentials="$(mktemp)";
+cat > "$awsSharedCredentialsFileForInitialCredentials" <<EOF
+[$initialProfileName]
+EOF
+if [ -n "${AWS_ACCESS_KEY_ID:-}" ]; then
+cat >> "$awsSharedCredentialsFileForInitialCredentials" <<EOF
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+EOF
+fi
+if [ -n "${AWS_SECRET_ACCESS_KEY:-}" ]; then
+cat >> "$awsSharedCredentialsFileForInitialCredentials" <<EOF
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
+fi
+chmod 666 "$awsSharedCredentialsFileForInitialCredentials"; # -r--------
+
+# hide original env credentials (they have higher precedence than AWS_DEFAULT_PROFILE and would otherwise override that)
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+
+# generate config file for role assumption
+awsConfigFileForAssumeRole="$(mktemp)";
+cat > "$awsConfigFileForAssumeRole" <<EOF
+[profile $assumeRoleProfileName]
+role_arn = $arnOfRoleToAssume
+source_profile = $initialProfileName
+EOF
+chmod 666 "$awsConfigFileForAssumeRole"; # -r--r--r--
+
+# run with assume-role AWS profile
+export AWS_DEFAULT_PROFILE=$assumeRoleProfileName
+export AWS_SHARED_CREDENTIALS_FILE=$awsSharedCredentialsFileForInitialCredentials
+export AWS_CONFIG_FILE=$awsConfigFileForAssumeRole
+/usr/bin/aws "$@"
