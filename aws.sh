42472518c83895ea05a949784d8fb8244d669dea
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-07-07 00:23:08 +0200

    cleanup

--- aws.sh
+++ aws.sh
@@ -6,7 +6,7 @@ randomRoleSessionName=$(date +%s)
 roleSessionName=${ASSUME_ROLE_SESSION_NAME:-$randomRoleSessionName}
 
 eval "$(/usr/local/bin/aws sts assume-role --role-session-name="$roleSessionName" --role-arn="$arnOfRoleToAssume" --output text --query='
-  join(`";"`, [
+  join(`"\n"`, [
     join(``, [`"export AWS_ACCESS_KEY_ID="`, Credentials.AccessKeyId]),
     join(``, [`"export AWS_SECRET_ACCESS_KEY="`, Credentials.SecretAccessKey]),
     join(``, [`"export AWS_SESSION_TOKEN="`, Credentials.SessionToken])
