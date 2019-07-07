19f8e05d12ae1d2f925963754beeecfe860ab357
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-07-07 06:09:02 +0200

    simplify

--- aws.sh
+++ aws.sh
@@ -5,12 +5,17 @@ arnOfRoleToAssume=$ASSUME_ROLE_ARN
 randomRoleSessionName=$(date +%s)
 roleSessionName=${ASSUME_ROLE_SESSION_NAME:-$randomRoleSessionName}
 
-eval "$(/usr/local/bin/aws sts assume-role --role-session-name="$roleSessionName" --role-arn="$arnOfRoleToAssume" --output text --query='
-  join(`"\n"`, [
-    join(``, [`"export AWS_ACCESS_KEY_ID="`, Credentials.AccessKeyId]),
-    join(``, [`"export AWS_SECRET_ACCESS_KEY="`, Credentials.SecretAccessKey]),
-    join(``, [`"export AWS_SESSION_TOKEN="`, Credentials.SessionToken])
-  ])'
-)"
+temporaryCredentialsEnv() {
+  /usr/local/bin/aws sts assume-role \
+    --role-session-name="$roleSessionName" \
+    --role-arn="$arnOfRoleToAssume" \
+    --output text \
+    --query='Credentials.[
+      join(`=`, [`AWS_ACCESS_KEY_ID`, AccessKeyId]),
+      join(`=`, [`AWS_SECRET_ACCESS_KEY`, SecretAccessKey]),
+      join(`=`, [`AWS_SESSION_TOKEN`, SessionToken])
+    ]'
+}
 
-exec /usr/local/bin/aws "$@"
+assumeRoleEnv=$(temporaryCredentialsEnv)
+exec env $assumeRoleEnv /usr/local/bin/aws "$@"
